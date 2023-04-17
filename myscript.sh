0ca1571463fa7d08cab67e4092665e447912a906
ElKo3006 <130043333+ElKo3006@users.noreply.github.com>
2023-04-17 11:16:13 +0200

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bush
+cd Folder
+mkdir subfolder1 subfolder2 subfolder3
+cd subfolder1
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir lk1 lk2 lk3
+ls -la
+mv file1.txt file2.txt ../subfolder2
